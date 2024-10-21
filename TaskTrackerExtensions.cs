a9c83b5146c641bc622c5ab0775d1c48a6f441a2
caowens <caowens555@yahoo.com>
2024-10-20 22:44:03 -0700

    Add documentation comment to the utilities

--- TaskTrackerExtensions.cs
+++ TaskTrackerExtensions.cs
@@ -2,6 +2,12 @@ namespace TaskTracker.Utilities
 {
     class TaskTrackerExtensions
     {
+        /// <summary>
+        /// Tries to see parse a string into a Guid.
+        /// </summary>
+        /// <param name="guidString">The <see cref="string"/> you are trying to parse.</param>
+        /// <param name="parsedGuid">The <see cref="Guid"/> version of the given string.</param>/// 
+        /// <returns>true if the parse operation was successful; otherwise, false. If true, also can use the <see cref="parsedGuid"/> as a variable outside of the method.</returns>
         public static bool TryParseGuid(string guidString, out Guid parsedGuid) 
         {
             return Guid.TryParse(guidString, out parsedGuid);
